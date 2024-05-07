0091742699a5972a41d9ef6eae6afc1fb66ca83d
Bilal-Habib <dev.bilal01@gmail.com>
2024-05-07 15:40:38 +0100

    fix: tailwind css styling is now being applied

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
+    <>
+      <h1 className="head-text text-left p-24 m-20">Home</h1>
+    </>
   );
 }
