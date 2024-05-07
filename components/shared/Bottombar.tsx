7637253bf776f90199a34c8215e5494e3c11f2e4
Bilal-Habib <dev.bilal01@gmail.com>
2024-05-07 16:51:08 +0100

    chore: applied prettier to project

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Bottombar = () => {
-  return (
-    <div>Bottombar</div>
-  )
-}
+  return <div>Bottombar</div>;
+};
 
-export default Bottombar
+export default Bottombar;
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,22 +1,27 @@
-import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Topbar = () => {
   return (
-    <nav className='topbar'>
-      <Link href="/" className='flex items-center gap-4'>
-      <Image src="/assets/logo.svg" alt='logo' width={28} height={28} />
-      <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
 
-      <div className='flex items-center gap-1'>
-        <div className='block md:hidden'>
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
           <SignedIn>
             <SignOutButton>
-              <div className='flex cursor-pointer'>
-                <Image src="/assets/logout.svg" alt='logout' width={24} height={24} />
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                />
               </div>
             </SignOutButton>
           </SignedIn>
@@ -25,14 +30,13 @@ const Topbar = () => {
         <OrganizationSwitcher
           appearance={{
             elements: {
-              organizationSwitcherTrigger:
-              "py-2 px-4"
-            }
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
           }}
         />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
