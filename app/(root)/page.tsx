8468d025daff692d3b9f533ac703674078ce28ce
Bilal-Habib <dev.bilal01@gmail.com>
2024-05-09 16:09:40 +0100

    feat: create initial thread card structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
 import { currentUser } from "@clerk/nextjs";
 
@@ -9,7 +10,7 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className="head-text text-left p-24 m-20">Home</h1>
+      <h1 className="head-text text-left">Home</h1>
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
@@ -17,18 +18,17 @@ export default async function Home() {
         ) : (
           <>
             {result.posts.map((post) => (
-              <p>Thread Card</p>
-              // <ThreadCard
-              //   key={post._id}
-              //   id={post._id}
-              //   currentUserId={user?.id}
-              //   parentId={post.parentId}
-              //   content={post.text}
-              //   author={post.author}
-              //   community={post.community}
-              //   createdAt={post.createdAt}
-              //   comments={post.children}
-              // />
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
             ))}
           </>
         )}
